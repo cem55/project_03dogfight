26b974268d2b1bd5f9d2155028dd2a3de22eadf2
cem55 <chocowala@gmail.com>
2018-02-06 17:16:27 +0700

    Initial commit

---
+++ Assets/Glider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glider : MonoBehaviour {
+	public Camera cam;
+	public float movespeed;
+	public float acceleration;
+	public float turnspeed;
+
+	Rigidbody rb;
+	float speed;
+	Vector3 movevector;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float turnH = Input.GetAxis ("Mouse X");
+		float turnV = Input.GetAxis ("Mouse Y");
+		//Debug.Log (turnH + "," + turnV);
+
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		speed += moveV * acceleration;
+		if (movevector == Vector3.zero) {
+			speed = Mathf.Clamp (speed, -movespeed, movespeed);
+		} else {
+			speed = Mathf.Clamp (speed, 0f, movespeed);
+		}
+
+		movevector = transform.forward * speed;
+		Debug.Log (movevector);
+		rb.velocity = movevector;
+		//rb.AddForce (movevector);
+	}
+}
